f1e3d7d878a91adc9123c26cbb509899300c75d7
Charles Tabor <ct37@txstate.edu>
2021-10-16 22:12:34 -0500

    Remove dead code in texture module

--- src/texture.rs
+++ src/texture.rs
@@ -10,16 +10,6 @@ pub struct Texture {
 }
 
 impl Texture {
-    pub fn from_bytes(
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        bytes: &[u8],
-        label: &str,
-    ) -> Result<Self> {
-        let img = image::load_from_memory(bytes)?;
-        Self::from_image(device, queue, &img, Some(label))
-    }
-
     pub fn from_image(
         device: &wgpu::Device,
         queue: &wgpu::Queue,
